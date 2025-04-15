621ed1ef35ceadad2222f5a7e2507c4d80148fa1
Majid Soltanian <masotiofficial@gmail.com>
2025-04-15 21:12:33 +0330

    https://quera.org/problemset/16396

---
+++ P0016396/source.py
@@ -0,0 +1,17 @@
+# Problem link: https://quera.org/problemset/16396
+
+class Foo:
+    def __init__(self):
+        self._x = 0
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value >= 0:
+            self._x = value % 100
+
+        elif value < 0:
+            self._x = -1
---
+++ P0016396/test/sample_test.py
@@ -0,0 +1,22 @@
+import unittest
+from ..source import Foo
+
+
+class TestMethods(unittest.TestCase):
+    def setUp(self):
+        self.f = Foo()
+
+    def test_1(self):
+        self.assertEqual(self.f.x, 0)
+
+    def test_2(self):
+        self.f.x = 9
+        self.assertEqual(self.f.x, 9)
+        self.f.x = 0
+        self.assertEqual(self.f.x, 0)
+        self.f.x = 1
+        self.assertEqual(self.f.x, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
